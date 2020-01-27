af8c6672e1c302d7a3b0bcc2f80e4abca06bc967
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-01-27 23:52:16 +0200

    demo changes

--- demo-1/grpc-dotnet/client/Program.cs
+++ demo-1/grpc-dotnet/client/Program.cs
@@ -9,10 +9,12 @@ namespace client
     {
         static async Task Main(string[] args)
         {
+            Console.WriteLine("Enter name:");
+            var name = Console.ReadLine();
+
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client =  new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(
-                              new HelloRequest { Name = "GreeterClient" });
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
             Console.WriteLine("Greeting: " + reply.Message);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
